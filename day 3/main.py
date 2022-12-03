c76808d0d2fe1d05a2c8700f6cf950d3c63be584
TheRedMoon <Jeroen_vanvoorst@hotmail.com>
2022-12-03 12:02:51 +0100

    final day3 version used

--- day 3/main.py
+++ day 3/main.py
@@ -9,6 +9,24 @@ def convert_to_priority(char):
 
     return priority
 
+def det_r2(group):
+    x, y, z = group
+    x = set(x)
+    y = set(y)
+    z = set(z)
+    common_char = list(x.intersection(y, z))[0]  # can also use bitwise and set(x) & set(y) & set(z)
+    priority = convert_to_priority(common_char)
+    return priority
+
+def det_r1(line):
+    half = int(len(line)/2)
+    x,y = line[:half], line[half:]
+    x = set(x)
+    y = set(y)
+    res = list(x.intersection(y))[0]
+    priority = convert_to_priority(res)
+    return priority
+
 if __name__ == '__main__':
     with open("input.txt") as file:
         lines = [line.rstrip('\n') for line in file]
@@ -17,32 +35,16 @@ if __name__ == '__main__':
     total_prio_r2 = 0
     lines_found = 0
     group = []
-    list_of_groups = []
+    # loop only once for r2 and r1.
     for line in lines:
         #could've just used next and iter but too much thinking
         lines_found += 1
         group.append(line)
         if lines_found % 3 == 0:
-            list_of_groups.append(group)
-            group = []
+            total_prio_r2 += det_r2(group)
             lines_found = 0
-        half = int(len(line)/2)
-        x,y = line[:half], line[half:]
-        x = set(x)
-        y = set(y)
-        res = list(x.intersection(y))[0]
-        priority = convert_to_priority(res)
-        total_prio += priority
-
-    for group in list_of_groups:
-        x,y,z = group
-        x = set(x)
-        y = set(y)
-        z = set(z)
-        common_char = list(x.intersection(y, z))[0] #can also use bitwise and set(x) & set(y) & set(z)
-        priority = convert_to_priority(common_char)
-
-        total_prio_r2 += priority
+            group = []
+        total_prio += det_r1(line)
 
     print('priority r1', total_prio)
     print('priority r2', total_prio_r2)
